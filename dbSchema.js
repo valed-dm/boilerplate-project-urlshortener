9b39d68b851bd0cf8dd4231e9b3af03ba8fbf126
valed-dm <dmvaled@gmail.com>
2021-08-29 22:15:42 +0300

    mongodb Model created

--- dbSchema.js
+++ dbSchema.js
@@ -1,1 +1,10 @@
+var mongoose = require("mongoose");
+
+var urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number,
+  created: Date
+});
+
+module.exports = mongoose.model("urlLIST", urlSchema, "URLs");
 
